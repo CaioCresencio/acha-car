b6a3572bc3b25f33f7f59544cbf648ffb279ae5b
CaioCresencio <caiocresencio98@gmail.com>
2020-10-25 13:26:22 -0300

    Primeira versão

--- assets/js/language_control.js
+++ assets/js/language_control.js
@@ -1,12 +1,3 @@
-const home_p1_pt = `
-    Pensando em tornar o sistema de caronas das universidades brasileiras mais inclusivo e seguro, buscamos desenvolver um aplicativo vinculado que acabe com as filas de espera e ajude encontrar caronas facilmente.
-`;
-
-const home_p1_en = `
-    Seeking to make the ride system of brazilian universities more inclusive and safe, we will develop a app that eliminates waiting lines and helps find rides easily.
-`;
-
-
 function getTextID(text, id) {
     document.getElementById(`${id}`).innerHTML = text;
 }
@@ -19,13 +10,40 @@ function getTextClass(text, class_name) {
 window.addEventListener("load", function (event) {
 
     document.getElementById('btn_lang_en').addEventListener('click', function () {
-        getTextID(home_p1_en,'home_p1');
+        getTextID(home_p1_en, 'home_p1');
+        getTextID(home_p2_en, 'home_p2');
+        getTextID(home_p3_en, 'home_p3');
         
-    });
-    document.getElementById('btn_lang_pt').addEventListener('click', function () {
-        getTextID(home_p1_pt,'home_p1');
+        getTextID(feature1_en, 'feature1');
+        getTextID(feature2_en, 'feature2');
+        getTextID(feature3_en, 'feature3');
+        getTextID(feature4_en, 'feature4');
+        getTextID(feature5_en, 'feature5');
+        getTextID(feature6_en, 'feature6');
+
+        getTextID(title_feature_en,'title_feature')
+        getTextID(contact_title_en,'contact-title');
+        getTextID(popouver_content_en,'popover-description')
 
     });
-    
+    document.getElementById('btn_lang_pt').addEventListener('click', function () {
+        getTextID(home_p1_pt, 'home_p1');
+        getTextID(home_p2_pt, 'home_p2');
+        getTextID(home_p3_pt, 'home_p3');
+
+        getTextID(feature1_pt, 'feature1');
+        getTextID(feature2_pt, 'feature2');
+        getTextID(feature3_pt, 'feature3');
+        getTextID(feature4_pt, 'feature4');
+        getTextID(feature5_pt, 'feature5');
+        getTextID(feature6_pt, 'feature6');
+
+        getTextID(title_feature_pt,'title_feature');
+        getTextID(contact_title_pt,'contact-title');
+        getTextID(popouver_content_pt,'popover-description');
+
+
+    });
+
     document.getElementById('btn_lang_en').click();
 });
---
+++ assets/js/language_text.js
@@ -0,0 +1,90 @@
+const home_p1_pt = `
+    Pensando em tornar o sistema de caronas das universidades brasileiras mais inclusivo e seguro, buscamos desenvolver um aplicativo vinculado que acabe com as filas de espera e ajude encontrar caronas facilmente.
+`;
+const home_p2_pt = `
+Já pensou se houvesse uma solução que antecipasse o horário que você pegaria uma carona e você ficasse estudando nesse meio tempo?
+`;
+const home_p3_pt = `
+Pensando em tornar o sistema de caronas das universidades brasileiras mais inclusivo e seguro, buscamos desenvolver um aplicativo vinculado que acabe com as filas de espera e ajude encontrar caronas facilmente.
+`;
+
+
+const home_p1_en = `
+    We know that several times you had to interrupt your studies, or you just left the class running to catch up the public transport, didn’t you? And when you were waiting for a ride for a long time ...
+`;
+const home_p2_en = `
+    Have you ever thought if there was a way that preview the time you would take a ride and you were studying in the meanwhile?
+`;
+
+const home_p3_en = `
+    Seeking to make the ride system of brazilian universities more inclusive and safe, we will develop a app that eliminates waiting lines and helps find rides easily.
+`;
+
+
+const feature1_pt = `
+    Vincular com uma rede social para saber amigos em comum para propiciar uma melhor segurança ao usuário.
+`;
+
+const feature2_pt = `
+    Acessibilidade nas funcionalidades do aplicativo como leitor de tela, contrastes, aumentar ou diminuir a fonte.
+`;
+const feature3_pt = `
+    Cadastro para pessoas que possuem carro adaptado para inclusão de pessoas usuárias de cadeira de rodas.
+`;
+const feature4_pt = `
+    Feedback
+    <ul>
+        <li>Gamificação com vantagens para pessoas que dão carona mais frequentemente.</li>
+        <li>Por exemplo: ganhar vouchers em postos de gasolina de forma progressiva, ou seja, quem tiver uma melhor avaliação e feedbacks ganharia descontos maiores.</li>
+    </ul>
+`;
+const feature5_pt = `
+    Mapear pontos acessíveis dentro do campus para promover carona às pessoas com alguma deficiência.
+`;
+
+const feature6_pt = `
+    Um app para todos! Pessoas com deficiência conseguem encontrar caronas mais facilmente possuindo um atendimento preferencial e inclusão.
+`;
+
+const feature1_en = `
+    Link to a social network to know friends in common to provide better user security.
+`;
+
+const feature2_en = `
+    This app is all accessible, containing a screen reader, contrasts control, and options to increase or not the font.
+`;
+const feature3_en = `
+    Registration for people who have adapted car for inclusion of wheelchair users.
+`;
+
+const feature4_en = `
+    Feedback
+    <ul>
+        <li>Gamification of advantages for people who give more ride.</li>
+        <li>For example: win vouchers at gas stations progressively. Who has a better rating and feedbacks would get bigger discounts.</li>
+    </ul>
+`;
+
+const feature5_en = `
+    Map accessible points to offer a ride to people with disabilities.
+`;
+
+const feature6_en = `
+    An app for everyone! People with disabilities can find rides more easily with preferential customer service and inclusion.
+`;
+
+const title_feature_pt = `
+<span>Funcionalidades</span> do App
+`;
+
+const title_feature_en = `
+APP <span>Features</span>
+`;
+
+const contact_title_pt = `Pegue essa carona conosco :)`;
+  
+const contact_title_en = `Take this ride with us :)`;
+
+const popouver_content_pt = `“Acha!” é uma expressão muito utilizada em São Carlos - SP, quando algo é surpreendente  e inacreditável. E esse termo representa muito o nosso espírito inovador!`;
+
+const popouver_content_en = `"Acha!" is an expression widely used in São Carlos - SP, when something is surprising and unbelievable. And that term represents a lot of our innovative spirit!`;
